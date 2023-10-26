e7d4562dc498873bb8c2e208e23abf6595a7c98f
anh188 <linh188abc@gmail.com>
2023-10-26 09:01:25 +0700

    first commit

---
+++ js/price.js
@@ -0,0 +1,24 @@
+
+// window.onload = function () {
+//   slideMin();
+// };
+
+// const minVal = document.querySelector(".min-val");
+// const maxVal = document.querySelector(".max-val");
+// const priceInputMin = document.querySelector(".min-input");
+// const priceInputMax = document.querySelector(".max-input");
+// const minTooltip = document.querySelector(".min-tooltip");
+// const maxTooltip = document.querySelector(".max-tooltip");
+// const minGap = 0;
+// const range = document.querySelector(".slider-track");
+// const sliderMinValue = parseInt(minVal.min);
+// const sliderMaxValue = parseInt(maxVal.max);
+
+// function slideMin() {
+//   let gap = parseInt(maxVal.value)- parseInt(minVal.value);
+//   if(gap <= minGap) {
+//       minVal.value = parseInt(maxVal.value) - minGap;
+//   }
+//   minTooltip.innerHTML = "$" + minVal.value;
+//   priceInputMin.value=minVal.value;
+// }
